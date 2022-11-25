6953c7d85aa51c687dc42d4742d8955fda20097d
Anonymous <>
2022-11-24 21:52:30 -0500

    Finish aggregator

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [names, setNames] = useState([]);
   const [allData, setAllData] = useState([]);
   const [filteredData, setFilteredData] = useState(allData);
-  const [wishlist, setWishlist] = useState(Array(allData.length).fill(0));
+  const [wishlist, setWishlist] = useState([]);
   const [total, setTotal] = useState(0);
 
   // Get the character names from the API
@@ -24,6 +24,7 @@ function App() {
     let promises = [];
     names.forEach(name => {
       // Remove characters that are too new to have images or are not obtainable through the gacha system
+      // TODO: Add title to Collei: "Avidya Forest Ranger Trainee"
       if (name !== 'aloy' && name !== 'traveler-anemo' && name !== 'traveler-geo' && name !== 'traveler-electro' && name !== 'traveler-dendro') {
         promises.push(axios.get('https://api.genshin.dev/characters/' + name));
       }
@@ -31,6 +32,7 @@ function App() {
     Promise.all(promises).then(response => {
       setAllData(response.map(res => res.data));
       setFilteredData(response.map(res => res.data));
+      setWishlist(Array(response.length).fill(0));
     }).catch(error => {
       console.log(error);
     })
@@ -38,7 +40,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Genshin Impact Wishlist</h1>
+      <h1>Genshin Impact Wishlist Tracker</h1>
       <div className="appContainer">
         <div className="characters">
           {allData.map((item, index) => (
@@ -57,13 +59,47 @@ function App() {
           <h2>My Wishlist</h2>
             <p>
             Total: {total}&#9733;
+            {/* Resets the entire wishlist */}
+            <button onClick={
+              () => {
+                setWishlist(Array(wishlist.length).fill(0));
+                setTotal(0);
+              }
+            } className="charButton">Reset Cart</button>
               {wishlist.map((item, index) => {
                 if (item > 0) {
                   return (
                     <p>
-                      {item}x {allData[index].name}
+                      {item}x {allData[index].name} {}
+                      {/* Add a copy of a character to the wishlist */}
+                      <button onClick={
+                        () => {
+                          setTotal(total + allData[index].rarity);
+                          setWishlist((wishlist) => {
+                            let newWishlist = [...wishlist];
+                            newWishlist[index] += 1;
+                            return newWishlist;
+                          });
+                        }
+                      } className="charButton">+</button>
+                      {/* Remove a copy of a character from the wishlist */}
+                      <button onClick={ 
+                        () => {
+                          if (wishlist[index] > 0) {
+                            setTotal(total - allData[index].rarity);
+                          }
+                          setWishlist((wishlist) => {
+                            let newWishlist = [...wishlist];
+                            newWishlist[index] -= 1;
+                            return newWishlist;
+                          });
+                        }
+                       } className="charButton">-</button>
                     </p>
                   );
+                }
+                else {
+                  return null;
                 }
               })}
             </p>
--- src/components/Character.js
+++ src/components/Character.js
@@ -8,13 +8,25 @@ export default function Character(props) {
     });
   }
 
+  function removeFromWishlist() {
+    if (props.wishlist[props.index] > 0) {
+      props.setTotal(props.total - props.item.rarity);
+    }
+    props.setWishlist((wishlist) => {
+      let newWishlist = [...wishlist];
+      newWishlist[props.index] -= 1;
+      return newWishlist;
+    });
+  }
+
   return (
     <div className="char">
       <img src={ "https://api.genshin.dev/characters/" + props.item.name.replace(' ', '-').toLowerCase().replace('kamisato-', '').replace('kaedehara-', '').replace('sangonomiya-', '').replace('-shogun', '').replace('kujou-', '') + "/icon-big" } alt={ props.item.name } className="charImg"/>
-      <h2 className="charText">{ props.item.name }</h2>
+      <p className="charText-title">{ props.item.name }</p>
       <p className="charText"><i>{ props.item.title }</i></p>
       <p className="charText">{ props.item.rarity }&#9733;, { props.item.vision }, { props.item.weapon }, { props.item.nation }</p>
       <button onClick={ addToWishlist } className="charButton">Add to Wishlist</button>
+      <button onClick={ removeFromWishlist } className="charButton">Remove from Wishlist</button>
     </div>
   );
 }    
